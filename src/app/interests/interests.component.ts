c68816743d320d786526f99e9208926c337b32dc
im-christopher <im.christopher.vr@gmail.com>
2025-04-22 09:22:12 -0600

    my-cv

---
+++ src/app/interests/interests.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interests',
+  standalone: false,
+  templateUrl: './interests.component.html',
+  styleUrl: './interests.component.css'
+})
+export class InterestsComponent {
+
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCExS9k2lHf3jpAkogR9dGzuSEptDjFdtg",
+        authDomain: "mycv-f1c7a.firebaseapp.com",
+        projectId: "mycv-f1c7a",
+        storageBucket: "mycv-f1c7a.firebasestorage.app",
+        messagingSenderId: "1086865603987",
+        appId: "1:1086865603987:web:d381724747fc9bf950fc66"
+    }
+};
